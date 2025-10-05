45105b5fba46c82a4098bfbc3e86e47660f57fb6
santoshhosmani <111982936+santoshhosmani@users.noreply.github.com>
2025-10-05 16:20:04 +0530

    Update CalculateAreaOfCircle.java
    Updated the Area of circle formula in the class file

--- CalculateAreaOfCircle.java
+++ CalculateAreaOfCircle.java
@@ -9,7 +9,8 @@ public class CalculateAreaOfCircle {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter the radius:");
         radius = sc.nextDouble();
-        area=2*3.14*radius*radius;
+        // use the formula to calculate the Area of circle using ==> pie*r*r
+        area=3.14*radius*radius;
         // entering the area in cm2 use number lock on+alt+0178==> to get as cm²
         System.out.println("the calculated cricle is :"+area+"cm²");
     }
